24bd21358f29ee85c1ff485924343599bbfda85b
U1traVeno <happ1less1917@gmail.com>
2024-11-27 23:37:36 +0800

    feat: login, register, get user info.

--- biz/dal/model/user.go
+++ biz/dal/model/user.go
@@ -1,5 +1,13 @@
 package model
 
+import (
+	"gorm.io/gorm"
+)
+
 type User struct {
-	// Your code here
+	gorm.Model
+	Username string `gorm:"unique"`
+	Password string `gorm:"not null"`
+	Role     string `gorm:"default:'user'"`
+	Email    string `gorm:"unique"`
 }
