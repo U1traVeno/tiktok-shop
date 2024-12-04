623659a491f180b0d68293312a9004aecb404a0c
xiaowan <xiaowan2310@gmail.com>
2024-12-04 15:53:40 +0800

    update: update addItem service

--- biz/service/cart_service.go
+++ biz/service/cart_service.go
@@ -2,11 +2,13 @@ package service
 
 import (
 	"context"
+	"errors"
 	"fmt"
 	"github.com/U1traVeno/tiktok-shop/biz/dal/model"
 	query "github.com/U1traVeno/tiktok-shop/biz/dal/query/cart"
 	"github.com/U1traVeno/tiktok-shop/biz/model/cart"
 	"github.com/cloudwego/hertz/pkg/app"
+	"gorm.io/gorm"
 )
 
 type CartService struct {
@@ -21,24 +23,39 @@ func NewCartService(ctx context.Context, c *app.RequestContext) *CartService {
 	}
 }
 
-func (s *CartService) AddItem(req *cart.AddItemReq) (*cart.AddItemResp, error) {
-	//判断商品数量
-	if req.Item.Quantity <= 0 {
-		return nil, fmt.Errorf("invalid quantity")
+func (s *CartService) AddItem(req *cart.AddItemReq) error {
+	//判断数据合法性
+	if req.Item.Quantity <= 0 || req.Item.ProductId <= 0 || req.UserId <= 0 {
+		return fmt.Errorf("invalid data")
 	}
 
-	//提供商品信息
-	item := &model.Cart{
-		UserId:    req.UserId,
-		ProductID: req.Item.ProductId,
-		Quantity:  req.Item.Quantity,
-	}
-
-	//向数据库添加购物车商品信息
 	cartTable := query.Cart
-	_ = cartTable.Create(item)
 
-	return nil, nil
+	//查找用户购物车是否已存在该商品
+	item, err := cartTable.Where(cartTable.UserId.Eq(req.UserId), cartTable.ProductID.Eq(req.Item.ProductId)).First()
+	if err != nil {
+		//未找到则向数据库添加该商品数据
+		if errors.Is(err, gorm.ErrRecordNotFound) {
+			item := &model.Cart{
+				UserId:    req.UserId,
+				ProductID: req.Item.ProductId,
+				Quantity:  req.Item.Quantity,
+			}
+			err = cartTable.Create(item)
+			if err != nil {
+				return fmt.Errorf("failed to create cart item: %w", err)
+			}
+			return nil
+		}
+		//其他报错返回错误
+		return fmt.Errorf("failed to find cart item: %w", err)
+	}
+	//更新商品数量
+	_, err = cartTable.Where(cartTable.UserId.Eq(req.UserId), cartTable.ProductID.Eq(req.Item.ProductId)).Update(cartTable.Quantity, req.Item.Quantity+item.Quantity)
+	if err != nil {
+		return fmt.Errorf("failed to update cart item: %w", err)
+	}
+	return nil
 }
 
 func (s *CartService) GetCart(req *cart.GetCartReq) (*cart.GetCartResp, error) {
